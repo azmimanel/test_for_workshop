f791ff7dde221cdb802e502165b6cf3f31f37e0f
azmim <azmimanel777@gmail.com>
2023-11-23 20:54:06 +0100

    three instead of two

---
+++ main.js
@@ -0,0 +1,8 @@
+// Function to compute the product of p1 and p2
+function myFunction(p1, p2) {
+    return p1 * p2;
+}
+
+const result = myFunction(2, 2);
+const resultTwo = myFunction(3, 3);
+console.log(resultTwo);
